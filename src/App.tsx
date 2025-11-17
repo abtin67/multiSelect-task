d56d054aa64de2a360a197ccb2adc3aa47a94235
abtin67 <andishmandabtin8@gmail.com>
2025-11-17 11:49:44 +0330

    Fix index.html configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 import MultiSelect from './components/Multiselect';
 
 function App() {
   return (
+    
     <div >
       <MultiSelect />
     </div>
+    
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Toaster } from 'react-hot-toast';
+import { HashRouter } from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <HashRouter>
     <App />
     <Toaster />
-  </React.StrictMode>
+  </HashRouter>
 );
 
 

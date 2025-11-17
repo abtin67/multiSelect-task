0228c0c96164b09e0fd73b94ac50a2e345896771
abtin67 <andishmandabtin8@gmail.com>
2025-11-17 13:28:20 +0330

    installing app router

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Toaster } from 'react-hot-toast';
-import { HashRouter } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
+
 
 
 const root = ReactDOM.createRoot(
